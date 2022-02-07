39193e153c30a229aabb3eb66d1bc467b3f8ad4a
SDWoo <damjang67@naver.com>
2022-02-07 20:57:13 +0900

    2/7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import Button from './components/Button';
+
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
+  `;
+
+const palette = {
+  blue: '#228be6',
+  gray: '#496057',
+  pink: '#f06595'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={{ palette }}>
+      <AppBlock>
+        <Button>Button</Button>
+        <Button color='gray'>Button</Button>
+        <Button color='pink'>Button</Button>
+      </AppBlock>
+    </ThemeProvider>
+  )
 }
 
 export default App;
