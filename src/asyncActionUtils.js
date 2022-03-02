f846acf449eae635c098c3de867d4447b3fb2bea
SDWoo <damjang67@naver.com>
2022-03-03 02:17:10 +0900

    context

---
+++ src/asyncActionUtils.js
@@ -0,0 +1,78 @@
+export default function createAsyncDispatcher(type, promiseFn) {
+  // 성공, 실패에 대한 액션 타입 문자열을 준비합니다.
+  const SUCCESS = `${type}_SUCCESS`;
+  const ERROR = `${type}_ERROR`;
+
+  // 새로운 함수를 만듭니다.
+  // ...rest 를 사용하여 나머지 파라미터를 rest 배열에 담습니다.
+  async function actionHandler(dispatch, ...rest) {
+    dispatch({ type }); // 요청 시작됨
+    try {
+      const data = await promiseFn(...rest); // rest 배열을 spread 로 넣어줍니다.
+      dispatch({
+        type: SUCCESS,
+        data
+      }); // 성공함
+    } catch (e) {
+      dispatch({
+        type: ERROR,
+        error: e
+      }); // 실패함
+    }
+  }
+
+  return actionHandler; // 만든 함수를 반환합니다.
+}
+
+export const initialAsyncState = {
+  loading: false,
+  data: null,
+  error: null
+};
+
+const loadingState = {
+  loading: true,
+  data: null,
+  error: null
+};
+
+const success = data => ({
+  loading: false,
+  data,
+  error: null
+});
+
+const error = e => ({
+  loading: false,
+  data: null,
+  error: e
+});
+
+export function createAsyncHandler(type, key) {
+  const SUCCESS = `${type}_SUCCESS`;
+  const ERROR = `${type}_ERROR`;
+
+  function handler(state, action) {
+    switch (action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: loadingState
+        };
+      case SUCCESS:
+        return {
+          ...state,
+          [key]: success(action.data)
+        };
+      case ERROR:
+        return {
+          ...state,
+          [key]: error(action.error)
+        };
+      default:
+        return state;
+    }
+  }
+
+  return handler;
+}
